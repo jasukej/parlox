520b9b6a90eb269936e7123e858b16a46a67a68b
Kezia <roselinakeziarijadi@gmail.com>
2026-03-21 17:07:11 -0700

    chore: scaffold rest of frontend + error parsing

--- src/compiler.c
+++ src/compiler.c
@@ -1,24 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-    initScanner(source);
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
 
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("    | ");
-        }
-        // length is limited by character precision as no terminator enforces the end
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+Parser parser;
 
-        if (token.type == TOKEN_EOF) break;
+static void errorAt(Token* token, const char* message) {
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type==TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
     }
+
+    fprintf(stderr, " at '%.*s'", token->length, token->start);
+    parser.hadError = true;
+}
+
+static void errorAtCurrent(const char* message) {
+    errorAt(&parser.current, message);
+}
+
+static void advance() {
+    parser.previous = parser.current;
+
+    // Report all errors recorded by the scanner
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+static void consume(TokenType type, const char* message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
+// Performs single pass compilation to output bytecode
+bool compile(const char* source, Chunk* chunk) {
+    initScanner(source);
+    
+    parser.hadError = false;
+    parser.panicMode = false;
+
+    advance();
+    // expression() or equivalent generation
+    consume(TOKEN_EOF, "Expect end of expression.");
+    return !parser.hadError;
 }
--- src/compiler.h
+++ src/compiler.h
@@ -1,6 +1,10 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 
-void compile(const char* source);
+#include "vm.h"
+
+// Compiles program from source to bytecode, and stores it in given chunk.
+// Returns whether compilation succeeded.
+bool compile(const char* source, Chunk* chunk);
 
 #endif
