70869d7b7bab0fa8299c980835941762dabb34ad
Kezia <roselinakeziarijadi@gmail.com>
2026-03-22 02:16:10 -0700

    feat: add runtime errors + logical ops

--- src/value.h
+++ src/value.h
@@ -23,7 +23,7 @@ typedef struct {
 #define AS_NUMBER(value) ((value).as.number)
 
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
-#define NIL_VAL(value) ((Value){VAL_NIL, {.number = 0}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 #define IS_BOOL(value) ((value).type == VAL_BOOL)
@@ -37,6 +37,7 @@ typedef struct {
     Value* values;
 } ValueArray;
 
+bool valuesEqual(Value a, Value b);
 void initValueArray(ValueArray* arr);
 void freeValueArray(ValueArray* arr);
 void writeValueArray(ValueArray* arr, Value value);
